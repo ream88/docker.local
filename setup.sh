2e9d90f331aa12bea3084db38ef35e17a72e90d3
Mario Uher <uher.mario@gmail.com>
2020-01-16 12:49:16 +0100

    Improve SD card handling

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 set -e
 
+if [ ! -d "/Volumes/boot" ]
+then
+    echo "❌ SD card is not mounted at /Volumes/boot"
+    exit 1
+fi
+
 printf "❓ Wi-Fi password: "
 read password
 
@@ -10,3 +16,11 @@ echo "✅ Copied Wi-Fi config"
 
 touch /Volumes/boot/ssh
 echo "✅ Enabled ssh"
+
+printf "❓ Unmount SD card? [y/N]: "
+read answer
+
+if [[ $answer =~ ^[Yy]$ ]]
+then
+    diskutil unmount /Volumes/boot
+fi
